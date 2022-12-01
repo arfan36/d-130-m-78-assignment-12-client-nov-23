57ebe3be72be767dac6f0b8ed3f4fdca0fd7c8df
arfan36 <arfanur36@gmail.com>
2022-12-01 19:28:58 +0600

    login info updated

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,119 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn, providerLogin } = useContext(AuthContext);
+    const [loginError, set_loginError] = useState('');
+    const [loginUserEmail, set_loginUserEmail] = useState('');
+    // token
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from?.pathname || '/';
+
+    // handle Login
+    const handleLogin = (data) => {
+        console.log(data);
+        const { email, password } = data;
+        set_loginError('');
+
+        // signIn With Email And Password
+        signIn(email, password).then((result) => {
+            const { user } = result;
+            console.log("🚀 ~ user", user);
+
+            // get user token
+            set_loginUserEmail(email);
+
+        }).catch((err) => {
+            console.error('err', err);
+            set_loginError(err.message);
+        });
+    };
+
+    // handle google login
+    const handleGoogleLogin = () => {
+        set_loginError('');
+        providerLogin().then((result) => {
+            const { user } = result;
+            console.log("🚀 ~ user", user);
+
+            // get user token
+            set_loginUserEmail(user.email);
+
+        }).catch((err) => {
+            console.error('err', err);
+            set_loginError(err.message);
+        });
+    };
+
     return (
         <div>
-            <h2>login</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit(handleLogin)}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email"
+                                        {
+                                        ...register("email", {
+                                            required: "Email Address is Required"
+                                        })
+                                        }
+                                        placeholder="email" className="input input-bordered" />
+                                    {
+                                        errors.email &&
+                                        <p className='text-error'>{errors.email?.message}</p>
+                                    }
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password"
+                                        {
+                                        ...register("password", {
+                                            required: "Password is Required",
+                                            minLength: { value: 6, message: 'Password must be 6 character or longer' }
+                                        })
+                                        }
+                                        placeholder="password" className="input input-bordered" />
+                                    {
+                                        errors.password &&
+                                        <p className='text-error'>{errors.password?.message}</p>
+                                    }
+                                    <label className="label">
+                                        <Link to="" className="label-text-alt link link-hover">Forgot password?</Link>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
+                                </div>
+                                <div>
+                                    {
+                                        loginError &&
+                                        <p className='text-error'>{loginError}</p>
+                                    }
+                                </div>
+                            </form>
+                            <p>New to here? <Link to={'/signup'} className="text-success">Create New Account</Link></p>
+                            <div className="divider">OR</div>
+                            <div onClick={handleGoogleLogin} className="btn btn-outline w-full">CONTINUE WITH GOOGLE</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -8,6 +8,13 @@ const auth = getAuth(app);
 const AuthProvider = ({ children }) => {
     const [user, set_user] = useState(null);
     const [loading, set_loading] = useState(true);
+
+    // signIn with google
+    const googleProvider = new GoogleAuthProvider();
+    const providerLogin = () => {
+        set_loading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
 
     // create user with email and password
     const createUser = (email, password) => {
@@ -45,6 +52,7 @@ const AuthProvider = ({ children }) => {
     }, []);
 
     const authInfo = {
+        providerLogin,
         createUser,
         signIn,
         updateUser,
