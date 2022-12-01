7cb20780eb4b80f0a2a687c4b283bb220a60a66b
arfan36 <arfanur36@gmail.com>
2022-12-01 14:51:51 +0600

    carousel added on home

---
+++ src/Pages/Home/Carousel/Carousel.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import google from '../../../assets/image/google.jpg';
+import iphone from '../../../assets/image/iphone.jpg';
+import samsung from '../../../assets/image/samsung.jpg';
+
+const Carousel = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={google} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={iphone} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={samsung} className="w-full" alt='' />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Carousel from '../Carousel/Carousel';
 
 const Home = () => {
     return (
         <div>
-            <h2>home</h2>
+            <Carousel></Carousel>
         </div>
     );
 };
