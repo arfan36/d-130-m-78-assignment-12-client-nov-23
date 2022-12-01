bf028884fedcae1f8be606a04b915054001da16a
arfan36 <arfanur36@gmail.com>
2022-12-01 14:10:18 +0600

    navbar info added

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut().then(() => {
+        }).catch((err) => {
+            console.error('err', err);
+        });
+    };
+
+    const menuItems = <>
+        <li><Link to={'/'}>Home</Link></li>
+        <li><Link to={'/brand-name'}>Brand Name</Link></li>
+        <li><Link to={'/about'}>About</Link></li>
+        <li><Link to={'/blog'}>Blog</Link></li>
+        {
+            user?.uid ?
+                <>
+                    <li><Link to={'/dashboard'}>Dashboard</Link></li>
+                    <li><button onClick={handleLogOut}>Sign out</button></li>
+                </>
+                : <li><Link to={'/login'}>Login</Link></li>
+        }
+    </>;
+
     return (
-        <div>
-            <h2>Navbar</h2>
+        <div className="navbar bg-base-100 flex justify-between">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {menuItems}
+                    </ul>
+                </div>
+                <Link to={'/'} className="btn btn-ghost normal-case text-xl">Buy Old Phone</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
+            <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+            </label>
         </div>
     );
 };
