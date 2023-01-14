52a536de349395586acc7dd26a510ebd1369b06c
arfan36 <arfanur36@gmail.com>
2023-01-14 18:46:54 +0600

    use useQuery, cause spinner problem

--- src/Pages/Home/Categories/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories/Categories.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import Loading from '../../../Shared/Loading/Loading';
 import Category from '../Category/Category';
 
 const Categories = () => {
-    const [categories, set_categories] = useState([]);
-    useEffect(() => {
-        fetch(`https://d-130-1-m-78-assignment-12-server-nov-23.vercel.app/category`, {
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-            .then(res => res.json())
-            .then(data => set_categories(data));
-    }, []);
+    const { data: categories, isLoading } = useQuery({
+        queryKey: ['category'],
+        queryFn: async () => {
+            const res = await fetch(`https://d-130-1-m-78-assignment-12-server-nov-23.vercel.app/category`, {
+                headers: {
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
+            const data = await res.json();
+            return data;
+        }
+    });
 
+    if (isLoading) {
+        return <Loading></Loading>;
+    }
 
     return (
-        <div className='mt-16'>
+        <div className='mt-8'>
             <h2 className="text-3xl text-center">Category</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center mb-16 mt-8 mx-8'>
                 {
+                    categories &&
                     categories.map(category => <Category
                         key={category._id}
                         category={category}
